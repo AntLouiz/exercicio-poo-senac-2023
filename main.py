c9887b8dd16ffce5cc80f3e72f5b08d060706b4b
Luiz Gonzaga <luizrodrigogogo@gmail.com>
2023-01-31 20:57:02 -0300

    Adiciona método novo para exibição do produto

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ class Produto:
         self.preco = preco
         self.peso = peso
 
+    def exibir(self):
+        print(f'Nome: {self.nome} - Preço: {self.preco}')
+
 produto = Produto('Massa de pizza', 5, 400)
 quantidade = int(input('Informe a quantidade: '))
 print(quantidade)
