96ea90ad725aa8c9818ce0c9a3edb1acef579682
Luiz Gonzaga <luizrodrigogogo@gmail.com>
2023-01-31 20:41:33 -0300

    Adiciona novo atributo
    Foi adicionado um atributo novo para que
    seja possível o cálculo do peso

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 class Produto:
-    def __init__(self, nome, preco):
+    def __init__(self, nome, preco, peso):
         self.nome = nome
         self.preco = preco
+        self.peso = peso
 
 quantidade = int(input('Informe a quantidade: '))
 print(quantidade)
